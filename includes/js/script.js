8d42c0274906bd88c50c28c2c9362c6c8441aeea
registercosmo <cosmoqiu@Cosmos-MacBook-Pro.local>
2014-08-20 22:07:27 -0700

    In this version, we add a modal windows — This windows pops up when a button is clicked — There is a form inside this windows — Some tips would show up when the mouse is placed on some links — When the red button is clicked, messages show up like tips

--- includes/js/script.js
+++ includes/js/script.js
@@ -18,5 +18,13 @@ $(function() {
     $('#successAlert').slideDown();
     
   });
+    
+  $('a.pop').click(function(e) {
+    e.preventDefault();
+  });
+    
+  $('a.pop').popover();
+    
+  $('[rel="tooltip"]').tooltip();
   
 });
